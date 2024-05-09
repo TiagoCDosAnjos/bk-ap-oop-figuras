bb0d53e57399a7471ae68ca45161e96365d4a77b
Tiago Correa dos Anjos <tiagocdosanjos@gmail.com>
2024-05-09 17:54:21 -0300

    Ok.

---
+++ OopFiguras/OopFiguras/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OopFiguras;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Triangulo triangulo;
+        Console.Write("Diagite o valor de X: ");
+        int x= int.Parse(Console.ReadLine());
+
+        Console.Write("Diagite o valor de Y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.Write("Diagite o valor de Z: ");
+        int z = int.Parse(Console.ReadLine());
+
+        triangulo = new Triangulo(x, y, z);
+
+        triangulo.TipoDeTriangulo();
+        Console.WriteLine();
+
+        Console.WriteLine(triangulo.ToString());
+    }
+}
---
+++ OopFiguras/OopFiguras/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopFiguras
+{
+    public class Triangulo
+    {
+        public int LadoX { get; set; }
+        public int LadoY { get; set; }
+        public int LadoZ { get; set; }
+
+        public string Tipo { get; set; }
+
+        public Triangulo(int ladoX, int ladoY, int ladoZ)
+        {
+            LadoX = ladoX;
+            LadoY = ladoY;
+            LadoZ = ladoZ;
+        }
+
+        public void TipoDeTriangulo()
+        {
+            if (LadoX == LadoY && LadoX == LadoZ)
+            {
+                Tipo = "Equilátero: Todos os lados iguais.";
+            }else if (LadoX == LadoY || LadoX == LadoZ || LadoY == LadoZ)
+            {
+                Tipo = "Isósceles: Dois lados iguais.";
+            }
+            else
+            {
+                Tipo = "Escaleno: Todos os lados diferentes.";
+            }
+        }
+
+        public override string ToString()
+        {
+            return Tipo;
+        }
+    }
+}
